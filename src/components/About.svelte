aa7cff5fd57b3f51a1be2456cdd94b76db8ca232
M Dragonborn <mdragonborn007@gmail.com>
2025-09-06 04:27:10 +0600

    grammer checked and paraphrased

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -5,18 +5,23 @@
 <section>
 <SectionHeader icon={faAddressCard} text="About Me" />
 	<p>
-		Hi im M_Dragonborn Or Musfiq Drake whichever way you know me im a 22-year-old <span class="highlight">CS</span> student From California.<br>
-		I run a industrial machine manufactering company with my family as my primary career and work on various small communitry projects as a hobby
-		and occasionally do freelance work.
-    	I am a audiophile & a gamer whos Currently learning programming a bit too seriously.
-    	I have knowledge in a few differnet programming languages like
-    	<span class="highlight">Python</span>,
-    	<span class="highlight">C</span>, <span class="highlight">HTML</span> and <span class="highlight">CSS</span>,
-    	with the
-    	first being my most and last being my least used languages.I try to enjoy my free time by playing videogames some of which you can see on my <a class="highlight" href="#steam">steam</a> I also play <a class="highlight" href="#chess">chess</a> and <span class="highlight">Dungeons and Dragons</span>.
-		some of my favourite games include ,<span class="highlight">Skyrim</span>, <span class="highlight">The Witcher series</span>, and <span class="highlight">Dark Souls</span>. I also love reading books my favourite being <span class="highlight">Song of Ice and Fire</span> series. (hence the obsession with dragons)
-		I generally hang around on
-		<a class="highlight" href="#discord">discord</a> but you can probably find me via <a class="highlight" href="#mail">mail</a>.
+		Hello there! My name is M_Dragonborn. I'm a 22-year-old <span class="highlight">computer science</span>
+		student from California.<br />
+		My primary career is managing an industrial machinery manufacturing company with my family. As a hobby,
+		I work on various open-source community projects, and I also do freelance work periodically.
+		I am a gamer & an audiophile who is currently learning programming a bit too seriously. I have knowledge
+		in a few different programming languages like
+		<span class="highlight">Python</span>,
+		<span class="highlight">C</span>, <span class="highlight">HTML</span>, and
+		<span class="highlight">CSS</span>, with the first being my most used and the last being my least used
+		language.<br/> You can see some of the video games I play on my
+		<a class="highlight" href="#steam">Steam</a> account. In addition, I also play <a class="highlight" href="#chess">chess</a> and
+		<span class="highlight">Dungeons and Dragons</span>. <span class="highlight">Skyrim</span>, <span class="highlight">The Witcher trilogy</span>, and
+		<span class="highlight">Dark Souls</span> are a few of my favorite games. I also enjoy reading, and
+		<span class="highlight">the Song of Ice and Fire</span>
+		collection is one of my favorites.<br/> (You can probably figure out the obsession with dragons by now).<br/> I generally hang around on
+		<a class="highlight" href="#discord">Discord</a> but you can probably find me via
+		<a class="highlight" href="#mail">mail</a>.
 	</p>
 </section>
 <style>
