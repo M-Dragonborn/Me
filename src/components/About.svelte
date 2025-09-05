8d6ec8ab0e197c37c9d5b7435ac16ffaeba24da6
M Dragonborn <mdragonborn007@gmail.com>
2025-09-05 23:34:32 +0600

    the proper start of hell hopefully

---
+++ src/components/About.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
+	import SectionHeader from '../components/SectionHeader.svelte';
+</script>
+<section>
+<SectionHeader icon={faAddressCard} text="About Me" />
+	<p>
+		Hi im M_Dragonborn Or Musfiq Drake whichever way you know me im a 22-year-old <span class="highlight">CS</span> student From California.<br>
+		I run a industrial machine manufactering company with my family as my primary career and work on various small communitry projects as a hobby
+		and occasionally do freelance work.
+    	I am a audiophile & a gamer whos Currently learning programming a bit too seriously.
+    	I have knowledge in a few differnet programming languages like
+    	<span class="highlight">Python</span>,
+    	<span class="highlight">C</span>, <span class="highlight">HTML</span> and <span class="highlight">CSS</span>,
+    	with the
+    	first being my most and last being my least used languages.I try to enjoy my free time by playing videogames some of which you can see on my <a class="highlight" href="#steam">steam</a> I also play <a class="highlight" href="#chess">chess</a> and <span class="highlight">Dungeons and Dragons</span>.
+		some of my favourite games include ,<span class="highlight">Skyrim</span>, <span class="highlight">The Witcher series</span>, and <span class="highlight">Dark Souls</span>. I also love reading books my favourite being <span class="highlight">Song of Ice and Fire</span> series. (hence the obsession with dragons)
+		I generally hang around on
+		<a class="highlight" href="#discord">discord</a> but you can probably find me via <a class="highlight" href="#mail">mail</a>.
+	</p>
+</section>
+<style>
+    a {
+        white-space: nowrap;
+    }
+
+    p {
+        text-align: justify;
+    }
+	
+    .highlight {
+        color: #48a0ff;
+    }
+	
+</style>
---
+++ src/components/Socials.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { faLink, faEnvelope, faChessKnight, faGamepad } from '@fortawesome/free-solid-svg-icons';
+	import {
+		faDiscord,
+		faGithub,
+		faReddit,
+		faTelegramPlane,
+		faTwitter,
+		faYoutube
+	} from '@fortawesome/free-brands-svg-icons';
+	import SectionHeader from '../components/SectionHeader.svelte';
+	import Social from '../components/Social.svelte';
+</script>
+
+<section>
+	<SectionHeader icon={faLink}  text="Socials" />
+	<ul>
+		<Social
+			faIcon={faDiscord}
+			link="https://discord.com/users/540170296406179851"
+			anchor="discord"
+			overrideText="M_Dragonborn"
+		/>
+		<Social faIcon={faGithub} link="https://github.com/M-Dragonborn" anchor="github" />
+		<Social faIcon={faTelegramPlane} link="https://t.me/MDragonborn" anchor="telegram" />
+		<Social
+			faIcon={faYoutube}
+			link="https://www.youtube.com/@mdragonborn"
+			anchor="youtube"
+		/>
+		<Social faIcon={faGamepad} link="https://steamcommunity.com/id/M-Dragonborn/" anchor="Steam" />
+		<Social faIcon={faChessKnight} link="https://www.chess.com/member/m_dragonborn" anchor="chess" />
+		<Social
+			faIcon={faEnvelope}
+			link="mailto:mdragonborn007@gmail.com"
+			anchor="mail"
+			overrideText="mdragonborn007@gmail.com"
+		/>
+	</ul>
+</section>
+
+<style lang="scss">
+	ul {
+		list-style: none;
+	}
+</style>
