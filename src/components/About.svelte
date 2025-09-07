2f283e4b2f25f03511b9b3d09324c43b14f1d2d7
M Dragonborn <mdragonborn007@gmail.com>
2025-09-07 22:22:42 +0600

    Update About.svelte

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -6,14 +6,13 @@
 <section>
 	<SectionHeader icon={faAddressCard} text="About Me" />
 	<p>
-		Hello there! My name is <span class="hightlight">M_Dragonborn</span>M_Dragonborn. I'm a 22-year-old <span class="highlight"
-			>computer science</span
-		>
+		Hello there! My name is <span class="highlight">M_Dragonborn</span>. I'm a 22-year-old
+		<span class="highlight">computer science</span>
 		student from California.<br />
 		My primary career is managing an industrial machinery manufacturing company owned by my family. As
-		a hobby, I work on various open-source community projects, and I also do freelance work every now and then.
-		I am a gamer & an audiophile who is currently learning programming a bit too seriously. I have knowledge
-		in a few different programming languages like
+		a hobby, I work on various open-source community projects, and I also do freelance work every now
+		and then. I am a gamer & an audiophile who is currently learning programming a bit too seriously.
+		I have knowledge in a few different programming languages like
 		<span class="highlight">Python</span>,
 		<span class="highlight">C</span>, <span class="highlight">HTML</span>, and
 		<span class="highlight">CSS</span>, with the first being my most used and the last being my
